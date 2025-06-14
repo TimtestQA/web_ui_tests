ae66e791a8d8a719c93def17a9c2dbdbe6bb188a
TimtestQA <feevoodoo@yandex.ru>
2025-06-14 20:48:04 +0800

    Fixpix

--- base/base_test.py
+++ base/base_test.py
@@ -3,6 +3,7 @@ from pages.news_feed_page.page import NewsFeedPage
 from base_components.sidebar.sidebar import Sidebar
 from pages.new_business_page.page import CreateNewBusinessPage
 from pages.messages_page.page import MessagesPage
+from pages.connect_page.page import ConnectPage
 from helpers.ui_helper import UIHelper
 
 class BaseTest:
@@ -16,6 +17,7 @@ class BaseTest:
         self.news_feed_page = lambda driver=self.driver: NewsFeedPage(driver)
         self.new_business_page = lambda driver=self.driver: CreateNewBusinessPage(driver)
         self.messages_page = lambda driver=self.driver: MessagesPage(driver)
+        self.connect_page = lambda driver=self.driver: ConnectPage(driver)
 
         # Components
         self.sidebar = lambda driver=self.driver: Sidebar(driver)
--- pages/connect_page/page.py
+++ pages/connect_page/page.py
@@ -2,4 +2,10 @@ import allure
 from base.base_page import BasePage
 
 class ConnectPage(BasePage):
+    _PAGE_URL = "https://example.com/connect"  # Замените на реальный URL
+
+    @allure.step("Check if connect page is opened")
+    def is_opened(self):
+        self.wait.until(lambda driver: "connect" in driver.current_url.lower())
+        return self
  
